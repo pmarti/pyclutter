2193985d125cb9dd6b071c135c477e4bf9da03d4
Emmanuele Bassi <ebassi@openedhand.com>
2006-06-23 16:04:36 +0000

    add py-compile

--- clutter/cluttermodule.c
+++ clutter/cluttermodule.c
@@ -15,14 +15,23 @@ init_clutter (void)
 
   init_pygobject ();
 
-  m = Py_InitModule ("_clutter", clutter_functions);
-  d = PyModule_GetDict (m);
+  g_warning ("initializing _clutter");
 
+  m = Py_InitModule ("_clutter", clutter_functions);
+  PyModule_AddObject (m, "__version__",
+		      Py_BuildValue ("(iii)",
+			             PYCLUTTER_MAJOR_VERSION,
+				     PYCLUTTER_MAJOR_VERSION,
+				     PYCLUTTER_MICRO_VERSION));
+  d = PyModule_GetDict (m);
   clutter_register_classes (d);
+
+  g_warning ("class registered");
 
   if (PyErr_Occurred ()) 
     {
       Py_FatalError ("can't initialise module clutter");
+      PyErr_Print ();
     }
   else
     clutter_init (NULL, NULL);
