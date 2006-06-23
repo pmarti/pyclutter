c0d91e776d12a85cb30a5e7163a38914d27c2e59
Emmanuele Bassi <ebassi@openedhand.com>
2006-06-23 17:32:24 +0000

    2006-06-23  Emmanuele Bassi  <ebassi@openedhand.com>
    	* clutter/cluttermodule.c: Remove the debug messages and fix the
    	__version__ tuple generation.

--- clutter/cluttermodule.c
+++ clutter/cluttermodule.c
@@ -15,23 +15,18 @@ init_clutter (void)
 
   init_pygobject ();
 
-  g_warning ("initializing _clutter");
-
   m = Py_InitModule ("_clutter", clutter_functions);
   PyModule_AddObject (m, "__version__",
 		      Py_BuildValue ("(iii)",
 			             PYCLUTTER_MAJOR_VERSION,
-				     PYCLUTTER_MAJOR_VERSION,
+				     PYCLUTTER_MINOR_VERSION,
 				     PYCLUTTER_MICRO_VERSION));
   d = PyModule_GetDict (m);
   clutter_register_classes (d);
 
-  g_warning ("class registered");
-
   if (PyErr_Occurred ()) 
     {
       Py_FatalError ("can't initialise module clutter");
-      PyErr_Print ();
     }
   else
     clutter_init (NULL, NULL);
