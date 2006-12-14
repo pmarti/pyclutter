c0d0dd253cf7fe96bc3f548b409baeaeb92348a9
Emmanuele Bassi <ebassi@openedhand.com>
2006-12-14 17:24:15 +0000

    2006-12-14  Emmanuele Bassi  <ebassi@openedhand.com>
    	* examples/rects.py:
    	* examples/behave.py: Update examples.

--- examples/rects.py
+++ examples/rects.py
@@ -17,7 +17,7 @@ def on_button_press_event (stage, event):
 
 stage = clutter.stage_get_default()
 stage.set_size(800,600)
-stage.set_color(0x6d, 0x6d, 0x70, 0xff)
+stage.set_color((0x6d, 0x6d, 0x70, 0xff))
 stage.connect('add', on_stage_add)
 stage.connect('button-press-event', on_button_press_event)
 
@@ -27,9 +27,9 @@ rect = None
 for i in range(1, 10):
     #rect = clutter.Rectangle(0x0000ff33)
     rect = clutter.Rectangle()
-    rect.set_color(0x35, 0x99, 0x2a, 0x66)
+    rect.set_color((0x35, 0x99, 0x2a, 0x66))
     rect.set_position((800 - (80 * i)) / 2, (600 - (60 * i)) / 2)
-    rect.set_size(80 * i,60 * i)
+    rect.set_size(80 * i, 60 * i)
     stage.add(rect)
     rect.show()
 
