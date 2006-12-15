090e6e570e89ac839b6b7bae098b91dd3ec5e5aa
Emmanuele Bassi <ebassi@openedhand.com>
2006-12-15 18:00:34 +0000

    2006-12-15  Emmanuele Bassi  <ebassi@openedhand.com>
    	* clutter/Makefile.am:
    	* clutter/keysyms.py: Add the key symbols list, and use the
    	pygtk lazy loading facility (currently, calls gtk._lazyutils
    	but might be useful to install our own copy?) to load what's
    	needed only when it's needed.

---
+++ clutter-gst/__init__.py
@@ -0,0 +1,1 @@
+from _clutter.gst import *
--- clutter/__init__.py
+++ clutter/__init__.py
@@ -6,6 +6,11 @@ from _clutter import *
 __version__ = _clutter.__version__
 MAX_ALPHA = _clutter.MAX_ALPHA
 
+# use the pygtk module lazy loading stuff
+from gtk._lazyutils import LazyNamespace, LazyModule
+
+keysyms = LazyModule('keysyms', locals())
+
 # provide pure-python implementations of the commodity alpha functions
 def ramp_func (alpha, data):
     """
