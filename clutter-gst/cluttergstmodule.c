38b6f8e239f875e82f04994804686466473c28df
Emmanuele Bassi <ebassi@openedhand.com>
2007-01-18 17:24:55 +0000

    2007-01-18  Emmanuele Bassi  <ebassi@openedhand.com>
    	* configure.ac: Bump to 0.2.0.
    
    	* clutter-gst/cluttergstmodule.c:
    	* clutter-gst/cluttergst.override:
    	* clutter-gst/Makefile.am: Desperately try to make the
    	ClutterGst bindings work; why, oh why Python does have
    	to suck dicks through a straw like this?

--- clutter-gst/cluttergstmodule.c
+++ clutter-gst/cluttergstmodule.c
@@ -3,6 +3,7 @@
 #endif
 
 #include <pygobject.h>
+#include <gst/gst.h>
 #include <clutter/clutter.h>
 #include <clutter-gst/clutter-gst.h>
 
@@ -10,14 +11,24 @@ void pycluttergst_register_classes (PyObject *d);
 extern PyMethodDef pycluttergst_functions[];
 
 DL_EXPORT (void)
-initcluttergst (void)
+init_gst (void)
 {
         PyObject *m, *d;
 
         init_pygobject ();
-        
-        m = Py_InitModule ("cluttergst", pycluttergst_functions);
+
+        if (PyImport_ImportModule ("clutter") == NULL) {
+                PyErr_SetString (PyExc_ImportError,
+                                 "could not import clutter");
+                return;
+        }
+
+        m = Py_InitModule ("_gst", pycluttergst_functions);
         d = PyModule_GetDict (m);
 
         pycluttergst_register_classes (d);
+
+        if (PyErr_Occurred ()) {
+                Py_FatalError ("unable to initialise cluttergst module");
+        }
 }
