a8b3c3594582e83f97ced94696d5fb293367a390
Emmanuele Bassi <ebassi@openedhand.com>
2007-07-28 19:54:43 +0000

    Remove pure-python alpha functions and hook into the C ones
    Use the alpha functions provided by the core C library instead of rewriting
    all of them using Python; we gain in speed and bug fixing. Also, writing them
    all would have been a maintainership disaster. Too bad that CPython API does
    not allow me to avoid cutting an pasting ten lines of code for each alpha
    function. Well: you win some, you lose some.

--- clutter/__init__.py
+++ clutter/__init__.py
@@ -1,6 +1,5 @@
 # -*- Mode: Python; py-indent-offset: 4 -*-
 
-from math import sin, pi
 from _clutter import *
 
 __version__ = _clutter.__version__
@@ -10,41 +9,3 @@ MAX_ALPHA = _clutter.MAX_ALPHA
 from gtk._lazyutils import LazyNamespace, LazyModule
 
 keysyms = LazyModule('keysyms', locals())
-
-# provide pure-python implementations of the commodity alpha functions
-def ramp_func (alpha, data):
-    """
-    Simple ramp function.
-    """
-    cur = alpha.get_timeline().get_current_frame()
-    frames = alpha.get_timeline().get_n_frames()
-    if cur < (frames / 2):
-        return cur * MAX_ALPHA / (frames / 2)
-    else:
-        return (frames - cur) * MAX_ALPHA / (frames / 2)
-
-def ramp_inc_func (alpha, data):
-    """
-    Simple monotonic increasing ramp function.
-    """
-    cur = alpha.get_timeline().get_current_frame()
-    frames = alpha.get_timeline().get_n_frames()
-    return cur * MAX_ALPHA / frames
-
-def ramp_dec_func (alpha, data):
-    """
-    Simple monotonic decreasing ramp function.
-    """
-    cur = alpha.get_timeline().get_current_frame()
-    frames = alpha.get_timeline().get_n_frames()
-    return (frames - cur) * MAX_ALPHA / frames
-
-def sine_func (alpha, data):
-    """
-    Simple sine waveform function.
-    """
-    cur = alpha.get_timeline().get_current_frame()
-    frames = alpha.get_timeline().get_n_frames()
-    x = cur * pi * 2 / frames
-    sine = (sin (x - (pi / 2)) + 1) * .5
-    return int (sine * MAX_ALPHA)
