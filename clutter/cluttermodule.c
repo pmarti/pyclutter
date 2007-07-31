1c4e1aaed0b723febddbceec58feebe378d29cc9
Emmanuele Bassi <ebassi@openedhand.com>
2007-07-31 10:11:31 +0000

    Register sink function for floating objects
    The Python GObject bindings need to register a sink function for the initially
    unowned object classes, so that they can take ownership of the objects and
    garbage collect them safely. Clutter has two classes with the initial floating
    reference, ClutterActor and ClutterAlpha. This patch registers a sink function
    for both of them. Thanks to Lauri Taimila for spotting this issue.

--- clutter/cluttermodule.c
+++ clutter/cluttermodule.c
@@ -11,12 +11,33 @@ void pyclutter_add_constants (PyObject *module, const gchar *prefix);
 
 extern PyMethodDef pyclutter_functions[];
 
+static void
+sink_clutteractor (GObject *object)
+{
+    if (g_object_is_floating (object)) {
+        g_object_ref_sink (object);
+    }
+}
+
+static void
+sink_clutteralpha (GObject *object)
+{
+    if (g_object_is_floating (object)) {
+        g_object_ref_sink (object);
+    }
+}
+
 DL_EXPORT (void)
 init_clutter (void)
 {
   PyObject *m, *d;
 
   init_pygobject ();
+
+  clutter_base_init ();
+
+  pygobject_register_sinkfunc (CLUTTER_TYPE_ACTOR, sink_clutteractor);
+  pygobject_register_sinkfunc (CLUTTER_TYPE_ALPHA, sink_clutteralpha);
 
   m = Py_InitModule ("_clutter", pyclutter_functions);
   PyModule_AddObject (m, "__version__",
