0cc2cca90521220e49f6e791e9f568c10238c5ba
Emmanuele Bassi <ebassi@openedhand.com>
2007-08-02 15:02:34 +0000

    Add more comments in the cairo-texture example

--- examples/cairo-texture.py
+++ examples/cairo-texture.py
@@ -18,19 +18,28 @@ def main ():
     cairo_tex.set_position((stage.get_width() - 200) / 2,
                            (stage.get_height() - 200) / 2)
 
+    # we obtain a cairo context from the cluttercairo.Texture
+    # and then we can use it with the cairo primitives to draw
+    # on it.
     context = cairo_tex.cairo_create()
-    context.scale(200, 200)
 
+    # we scale the context to the size of the surface
+    context.scale(200, 200)
     context.set_line_width(0.1)
     context.set_source_rgba(0, 0, 0, 1)
     context.translate(0.5, 0.5)
     context.arc(0, 0, 0.4, 0, pi * 2)
     context.stroke()
+
     del(context) # we need to destroy the context so that the
                  # texture gets properly updated with the result
+                 # of our operations
 
-    cairo_tex.rotate_y(45.0, cairo_tex.get_width() / 2,
-                             cairo_tex.get_height() / 2)
+    # cluttercairo.Texture is a clutter.Actor, so we can
+    # manipulate it as any other actor
+    cairo_tex.rotate_y(45.0,
+                       cairo_tex.get_width() / 2,
+                       cairo_tex.get_height() / 2)
 
     stage.add(cairo_tex)
     cairo_tex.show()
