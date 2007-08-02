e758f1123ac71259271a044510798c3beab3f6d8
Emmanuele Bassi <ebassi@openedhand.com>
2007-08-02 14:53:54 +0000

    Update cairo-texture example
    Use math.pi to draw the arc, and quit on button press.

--- examples/cairo-texture.py
+++ examples/cairo-texture.py
@@ -1,28 +1,30 @@
 import sys
+from math import pi
 
 import cairo
 import clutter
 from clutter import cluttercairo
 
+def on_button_press_event (stage, event):
+    clutter.main_quit()
+
 def main ():
-    # CREATE STAGE
     stage = clutter.stage_get_default()
     stage.set_color(clutter.Color(0xff, 0xcc, 0xcc, 0xff))
     stage.set_size(400,300)
+    stage.connect('button-press-event', on_button_press_event)
 
-    # CREATE CAIRO TEXTURE
     cairo_tex = cluttercairo.CairoTexture(200,200)
     cairo_tex.set_position((stage.get_width() - 200) / 2,
                            (stage.get_height() - 200) / 2)
 
-    # DO CAIRO DRAWING
     context = cairo_tex.cairo_create()
     context.scale(200, 200)
 
     context.set_line_width(0.1)
     context.set_source_rgba(0, 0, 0, 1)
     context.translate(0.5, 0.5)
-    context.arc(0, 0, 0.4, 0, 3.14 * 2)
+    context.arc(0, 0, 0.4, 0, pi * 2)
     context.stroke()
     del(context) # we need to destroy the context so that the
                  # texture gets properly updated with the result
@@ -30,7 +32,6 @@ def main ():
     cairo_tex.rotate_y(45.0, cairo_tex.get_width() / 2,
                              cairo_tex.get_height() / 2)
 
-    # SHOW CAIRO TEXTURE
     stage.add(cairo_tex)
     cairo_tex.show()
     stage.show()
