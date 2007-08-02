6db9767f82e5becb4b8c56983daf5cde369bb16b
Emmanuele Bassi <ebassi@openedhand.com>
2007-08-02 14:05:28 +0000

    Initial implementation of the clutter-gtk Python bindings
    The GtkClutter widget is a GtkSocket subclass that can embed the
    ClutterStage actor via the XEMBED protocol. For namespace purposes,
    it has been wrapped in Python under the cluttergtk.Embed class.
    
    This patch implements the cluttergtk.Embed widget bindings.

---
+++ clutter-gtk/cluttergtkmodule.c
@@ -0,0 +1,40 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <pygobject.h>
+#include <gtk/gtk.h>
+#include <clutter/clutter.h>
+#include <clutter-gtk/clutter-gtk.h>
+
+void pycluttergtk_register_classes (PyObject *d);
+extern PyMethodDef pycluttergtk_functions[];
+
+DL_EXPORT (void)
+initcluttergtk (void)
+{
+        PyObject *m, *d;
+
+        init_pygobject ();
+
+        if (PyImport_ImportModule ("clutter") == NULL) {
+                PyErr_SetString (PyExc_ImportError,
+                                 "could not import clutter");
+                return;
+        }
+
+        if (PyImport_ImportModule ("gtk") == NULL) {
+                PyErr_SetString (PyExc_ImportError,
+                                 "could not import gtk");
+                return;
+        }
+
+        m = Py_InitModule ("cluttergtk", pycluttergtk_functions);
+        d = PyModule_GetDict (m);
+
+        pycluttergtk_register_classes (d);
+
+        if (PyErr_Occurred ()) {
+                Py_FatalError ("unable to initialise cluttergtk module");
+        }
+}
