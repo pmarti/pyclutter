eb2e695c05e1b071df76c46f9bc5cc65d35931a4
Emmanuele Bassi <ebassi@openedhand.com>
2008-02-25 13:40:01 +0000

    2008-02-25  Emmanuele Bassi  <ebassi@openedhand.com>
    	* clutter/clutter.override: Fix error message for clutter.Stage()
    
    	* tidy/tidy-types.defs:
    	* tidy/tidy.override:
    	* tidy/tidymodule.c: Make tidy bindings actually work.

--- tidy/tidymodule.c
+++ tidy/tidymodule.c
@@ -11,25 +11,41 @@ void pytidy_register_classes (PyObject *d);
 
 extern PyMethodDef pytidy_functions[];
 
+static void
+sink_tidycellrenderer (GObject *object)
+{
+  if (g_object_is_floating (object))
+    g_object_ref_sink (object);
+}
+
+static void
+sink_tidylistcolumn (GObject *object)
+{
+  if (g_object_is_floating (object))
+    g_object_ref_sink (object);
+}
+
 DL_EXPORT (void)
 inittidy (void)
 {
-        PyObject *m, *d;
+  PyObject *m, *d;
 
-        init_pygobject ();
+  init_pygobject ();
 
-        if (PyImport_ImportModule ("clutter") == NULL) {
-                PyErr_SetString (PyExc_ImportError,
-                                 "could not import clutter");
-                return;
-        }
+  if (PyImport_ImportModule ("clutter") == NULL) {
+    PyErr_SetString (PyExc_ImportError, "could not import clutter");
+    return;
+  }
 
-        m = Py_InitModule ("tidy", pytidy_functions);
-        d = PyModule_GetDict (m);
+  pygobject_register_sinkfunc (TIDY_TYPE_CELL_RENDERER, sink_tidycellrenderer);
+  pygobject_register_sinkfunc (TIDY_TYPE_LIST_COLUMN, sink_tidylistcolumn);
 
-        pytidy_register_classes (d);
+  m = Py_InitModule ("tidy", pytidy_functions);
+  d = PyModule_GetDict (m);
 
-        if (PyErr_Occurred ()) {
-                Py_FatalError ("unable to initialise tidy module");
-        }
+  pytidy_add_constants (m, "TIDY_");
+  pytidy_register_classes (d);
+
+  if (PyErr_Occurred ())
+    Py_FatalError ("unable to initialise tidy module");
 }
