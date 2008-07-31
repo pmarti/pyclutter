319edabd06387f3648fdadcf27ff30ed9f49ba5b
Emmanuele Bassi <ebassi@openedhand.com>
2008-07-31 12:46:10 +0000

    2008-07-31  Emmanuele Bassi  <ebassi@openedhand.com>
    	* clutter-gst/Makefile.am:
    	* clutter-gst/__init__.py:
    	* clutter-gst/cluttergst.defs:
    	* clutter-gst/cluttergst.override:
    	* clutter-gst/cluttergstmodule.c: Overhaul the cluttergst module.
    	Because clutter-gst requires its own initialization function, we need
    	to put it into its own module, in order for it to have an __init__.py.
    	Thus, the way you import cluttergst has changed from this:
    
    		from clutter.cluttergst import cluttergst
    
    	to this:
    
    		import cluttergst
    
    	which is fairly more pythonic and solves the initialization problems
    	that we have.

--- clutter-gst/cluttergstmodule.c
+++ clutter-gst/cluttergstmodule.c
@@ -10,28 +10,90 @@
 void pycluttergst_register_classes (PyObject *d);
 extern PyMethodDef pycluttergst_functions[];
 
-DL_EXPORT (void)
-initcluttergst (void)
+/* clutter-gst requires its own initialization function, which calls the
+ * equivalent functions from GStreamer and Clutter in the right order.
+ */
+static void
+init_clutter_gst (void)
 {
-        PyObject *m, *d;
+  PyObject *av;
+  int argc, i;
+  char **argv;
 
-        init_pygobject ();
-
-        if (PyImport_ImportModule ("clutter") == NULL) {
-                PyErr_SetString (PyExc_ImportError,
-                                 "could not import clutter");
-                return;
+  av = PySys_GetObject ("argv");
+  if (!av)
+    {
+      if (!PyList_Check (av))
+        {
+	  PyErr_Warn (PyExc_Warning, "ignoring sys.argv: it must be a list of strings");
+	  av = NULL;
         }
-
-        m = Py_InitModule ("cluttergst", pycluttergst_functions);
-        d = PyModule_GetDict (m);
-
-        pycluttergst_register_classes (d);
-
-        if (PyErr_Occurred ()) {
-                Py_FatalError ("unable to initialise cluttergst module");
+      else
+        {
+          argc = PyList_Size (av);
+          for (i = 0; i < argc; i++)
+            {
+              if (!PyString_Check (PyList_GetItem (av, i)))
+                {
+		  PyErr_Warn (PyExc_Warning, "ignoring sys.argv: it must be a list of strings");
+                  av = NULL;
+                  break;
+		}
+            }
         }
-        else {
-                clutter_gst_init (NULL, NULL);
-        }
+    }
+
+  if (av != NULL)
+    {
+      argv = g_new (char*, argc);
+
+      for (i = 0; i < argc; i++)
+        argv[i] = g_strdup (PyString_AsString (PyList_GetItem (av, i)));
+    }
+  else
+    {
+      argc = 0;
+      argv = NULL;
+    }
+
+  if (!clutter_gst_init (&argc, &argv))
+    {
+      if (argv != NULL)
+        g_strfreev (argv);
+
+      PyErr_SetString (PyExc_RuntimeError, "cluttergst initialization error");
+
+	/* set the LC_NUMERIC locale back to "C", as Python < 2.4 requires
+	 * that it be set that way. */
+#if PY_VERSION_HEX < 0x020400F0
+      setlocale(LC_NUMERIC, "C");
+#endif
+      return;
+    }
+
+  /* set the LC_NUMERIC locale back to "C", as Python < 2.4 requires that
+   * it be set that way. */
+#if PY_VERSION_HEX < 0x020400F0
+  setlocale(LC_NUMERIC, "C");
+#endif
+
+  if (argv != NULL)
+    g_strfreev (argv);
+}
+
+DL_EXPORT (void)
+init_cluttergst (void)
+{
+  PyObject *m, *d;
+
+  init_pygobject ();
+
+  m = Py_InitModule ("_cluttergst", pycluttergst_functions);
+  d = PyModule_GetDict (m);
+
+  init_clutter_gst ();
+  pycluttergst_register_classes (d);
+
+  if (PyErr_Occurred ())
+    Py_FatalError ("unable to initialise cluttergst module");
 }
