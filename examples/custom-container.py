992de609cfc95dfd74c3c168532c67a787ec1184
Bastian Winkler <buz@netbuz.org>
2009-08-12 15:09:30 +0200

    Define Container.install_child_meta classmethod
    This extra classmethod allows to set a custom ChildMeta type for
    container actors. Also update the custom-container example to
    demonstrate this.

--- examples/custom-container.py
+++ examples/custom-container.py
@@ -1,8 +1,39 @@
 import gobject
 import clutter
 
-class SimpleBox(clutter.Actor, clutter.Container):
-    __gtype_name__ = 'SimpleBox'
+class SampleChildMeta(clutter.ChildMeta):
+    """
+    An example ChildMeta
+
+    This is an example ChildMeta implementation with only one property.
+    If 'allocate-hidden' is set True, the SampleBox container allocates
+    the actor's space if it's hidden.
+    """
+    __gtype_name__ = 'SampleChildMeta'
+    __gproperties__ = {
+            'allocate-hidden': (gobject.TYPE_BOOLEAN, 'Allocate hidden',
+                'Allocate space when hidden', True,
+                gobject.PARAM_CONSTRUCT|gobject.PARAM_READWRITE),
+    }
+    def __init__(self):
+        self._allocate_hidden = True
+
+    def do_get_property(self, pspec):
+        if pspec.name == 'allocate-hidden':
+            return self._allocate_hidden
+
+    def do_set_property(self, pspec, value):
+        if pspec.name == 'allocate-hidden':
+            self._allocate_hidden = value
+
+
+class SampleBox(clutter.Actor, clutter.Container):
+    """
+    An example Box container.
+
+    This is a very simple 'Box' container with a dynamic layout.
+    """
+    __gtype_name__ = 'SampleBox'
     __gproperties__ = {
             'orientation': (gobject.TYPE_INT, 'Orientation', 'Box orientation',
                 0, 1, 0, gobject.PARAM_READWRITE),
@@ -53,7 +84,8 @@ class SimpleBox(clutter.Actor, clutter.Container):
         min_width = 0
         natural_width = 0
         for child in self._children:
-            if not child.props.visible:
+            if not child.props.visible and not \
+                    self.child_get_property(child, 'allocate-hidden'):
                 continue
             child_min_width, child_natural_width = child.get_preferred_width(
                     for_height)
@@ -69,7 +101,8 @@ class SimpleBox(clutter.Actor, clutter.Container):
         min_height = 0
         natural_height = 0
         for child in self._children:
-            if not child.props.visible:
+            if not child.props.visible and not \
+                    self.child_get_property(child, 'allocate-hidden'):
                 continue
             child_min_height, child_natural_width = child.get_preferred_height(
                     for_width)
@@ -84,14 +117,17 @@ class SimpleBox(clutter.Actor, clutter.Container):
 
     def do_allocate(self, box, flags):
         child_x = child_y = 0
-        for actor in self._children:
-            w, h = actor.get_preferred_size()[2:]
+        for child in self._children:
+            if not child.props.visible and not \
+                    self.child_get_property(child, 'allocate-hidden'):
+                continue
+            w, h = child.get_preferred_size()[2:]
             child_box = clutter.ActorBox()
             child_box.x1 = child_x
             child_box.y1 = child_y
             child_box.x2 = child_box.x1 + w
             child_box.y2 = child_box.y1 + h
-            actor.allocate(child_box, flags)
+            child.allocate(child_box, flags)
             if self._orientation == self.HORIZONTAL:
                 child_x += w
             else:
@@ -106,28 +142,50 @@ class SimpleBox(clutter.Actor, clutter.Container):
         for actor in self._children:
             actor.paint()
 
+SampleBox.install_child_meta(SampleChildMeta)
+
 
 if __name__ == '__main__':
     def on_button_press(box, event):
         box.props.orientation = not box.props.orientation
         return True
+    def on_key_press(stage, event, box):
+        from clutter import keysyms
+        if event.keyval == keysyms.h:
+            for child in box:
+                do_allocate = box.child_get_property(child,
+                        'allocate-hidden')
+                box.child_set_property(child, 'allocate-hidden',
+                        not do_allocate)
+            box.queue_relayout()
 
     import random
     stage = clutter.Stage()
     stage.connect('destroy', clutter.main_quit)
 
-    box = SimpleBox()
-    for i in range(5):
+    box = SampleBox()
+    for i in range(10):
         rect = clutter.Rectangle()
         color = clutter.Color(random.randint(0, 255), random.randint(0, 255),
                 random.randint(0, 255), 255)
         rect.set_color(color)
         rect.set_size(50, 50)
         box.add(rect)
+        box.child_set_property(rect, 'allocate-hidden', False)
+        if i % 2:
+            rect.hide()
 
     stage.add(box)
     box.set_reactive(True)
+
+    info = clutter.Text()
+    info.set_text("Press 'h' to allocate hidden actors.\n" +
+            "Click the container actor to change it's orientation.")
+    stage.add(info)
+    info.set_y(stage.get_height() - info.get_height() - 10)
+
     box.connect('button-press-event', on_button_press)
+    stage.connect('key-press-event', on_key_press, box)
 
     stage.show()
     clutter.main()
